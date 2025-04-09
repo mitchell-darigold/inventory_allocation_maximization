abaae06be2019d42a2a086c48ce0d34936d62a8d
Darnell <midarne@internal.darigold.com>
2025-04-09 14:40:58 -0700

    cleaned up the spec_fix code and now it sends the result back to sqlite overwriting the existing mvp_inventory table

--- Python/spec_fix.py
+++ Python/spec_fix.py
@@ -1,27 +1,81 @@
+#there are many steps that need to happen to evolve this further.
+#I need to add a section that pulls the allowed_specs list automatically with a SQL query connection
+#I need to spit out the output back into sqlite3
+
 import pandas as pd
+import sqlite3
+
+#sqlite3 connection path
+sqlite3_db = 'inventory_allocation_maximization.db'
+sqlite3_conn_path = 'P:/sqlite3/sqlite-tools/'+sqlite3_db
+
+#f strings
+mvp_inventory_table = 'mvp_inventory'
+
+#sql queries:
+mvp_inventory_query = 'select * from mvp_inventory'
+
+def drop_table(table_name):
+    table = table_name
+    query = f'drop table if exists {table}'
+    return query
+
+def create_mvp_inventory(table_name):
+    table = table_name
+    query = f'''create table {table}(
+     AGE REAL,
+    TOTAL_WEIGHT REAL,
+    TOTAL_PALLETS REAL,
+    ITEM_NUMBER TEXT,
+    LOT_NO TEXT,
+    GRADE TEXT,
+    PRODUCTION_PLANT TEXT,
+    SPEC TEXT,
+    WHS_CODE TEXT,
+    SUBLOT_NO TEXT,
+    JOINER TEXT,
+    CLEANED_SPEC TEXT
+    );'''
+    return query
+
+try:
+    sqlite3_connection = sqlite3.connect(sqlite3_conn_path)
+    cursor = sqlite3_connection.cursor()
+    print('Successfully connected to the database')
 
 
-df = pd.read_csv('S:\Supply_Chain\Analytics\Inventory Allocation Maximization\MVP Data\inventory.csv')
-
-##print(df.head())
-
-#create a dictionary
-#eventually I will want to write SQL query that pulls from the item master table and generates a dictionary automatically
+    #df = pd.read_csv('S:\Supply_Chain\Analytics\Inventory Allocation Maximization\MVP Data\inventory.csv')
+    df = pd.read_sql_query(mvp_inventory_query, sqlite3_connection)
 
 
-allowed_specs = ['3002A', '3007DRYGUM', '3007FO', '3013A', '3025S', '3035A', '9999E', '9999A', '9999E']
+    allowed_specs = ['3002A', '3007DRYGUM', '3007FO', '3013A', '3025S', '3035A', '9999E', '9999A', '9999E']
 
-df['spec_listed'] = df['SPEC'].str.split('-')
+    df['spec_listed'] = df['SPEC'].str.split('-')
 
 
-def filter_specs(df, column_name, allowed_specs):
-    df[column_name] = df[column_name].apply(lambda x: [val for val in x if val in allowed_specs])
-    return df
+    def filter_specs(df, column_name, allowed_specs):
+        df[column_name] = df[column_name].apply(lambda x: [val for val in x if val in allowed_specs])
+        return df
 
-df = filter_specs(df, 'spec_listed', allowed_specs)
+    df = filter_specs(df, 'spec_listed', allowed_specs)
 
-df['CLEANED_SPEC'] = df['spec_listed'].apply(lambda x: '-'.join(map(str,x)))
+    df['CLEANED_SPEC'] = df['spec_listed'].apply(lambda x: '-'.join(map(str,x)))
 
-df = df.drop('spec_listed', axis=1)
+    df = df.drop('spec_listed', axis=1)
 
-df.to_csv('spec_test.csv',index=False)
+    try:
+        cursor.execute(drop_table(mvp_inventory_table))
+        cursor.execute(create_mvp_inventory(mvp_inventory_table))
+        df.to_sql(f'{mvp_inventory_table}', sqlite3_connection, if_exists='append', index=False)
+    except Exception as ex:
+        print(ex)
+
+    #df.to_csv('spec_test.csv',index=False)
+
+except sqlite3.Error as error:
+	print(error)
+
+finally:
+	if sqlite3_connection:
+		sqlite3_connection.close()
+		print("Connection to power_bi_data is closed")
